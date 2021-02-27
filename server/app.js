95a0992a90425a036cb0661227a9240538322362
John Robinson <johnarobinson133@gmail.com>
2021-02-27 12:26:54 -0700

    changed server app.js to hopfully fix routes

--- server/app.js
+++ server/app.js
@@ -10,11 +10,13 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")))
-}
-
 app.use('/posts', postRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  // app.use(express.static(path.join(__dirname, "../client/build")))
+  app.use(express.static("client/build"))
+  app.get("*",(req,res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")))
+}
 
 const CONNECTION_URL = 'mongodb+srv://jrobi133:Hunter15@workoutdb.izarp.mongodb.net/WorkoutDB?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
