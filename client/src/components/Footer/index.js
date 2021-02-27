3ca484366524c7450995794c514b87c23034ff06
Logan Coates <coates.logan1125@gmail.com>
2021-02-27 12:24:57 -0700

    footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import Nav from '../Nav'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { fab, faLinkedin, faDev, faGithub} from '@fortawesome/free-brands-svg-icons'
+import './style.css';
+import { Container } from '../Grid';
 // Add all icons to the library so you can use it in your page
 library.add(fas, far, fab, faLinkedin, faDev, faGithub)
 
 function Footer(){
   return (
-    <footer id="main-footer">
-      <Nav elementID="footer-nav">
-        <li className="nav-item"> 
-          <a className="nav-link" href="https://github.com/jrobi133/memory-games" target="_blank">
-            <FontAwesomeIcon icon={faGithub} size="2x" />
-          </a>
-        </li>
-      </Nav>
-    
-      {/* Footer Credits */}
-      <div id="footer-bottom">
-        <div id="footer-info">Copyright © 2019 | <a className="footer-link" href=".">Memory Games</a> |
-          <a className="footer-link" href="https://github.com/jrobi133/memory-games"> Coding Bootcamp</a>
+    <footer className="main-footer">
+      <Container className="footer">
+        <div className="footer-bottom">
+         <div className="footer-info">Copyright © 2021 | <a className="footer-link" href=".">Memory Games</a> |
+           <a className="footer-info" href="https://github.com/jrobi133/memory-games"> Github</a>
+
+           <a className="creators" href="https://github.com/jrobi133"> John Robinson </a>  | 
+           <a className="creators1" href="https://github.com/coatsey"> Logan Coates</a> |  
+           <a className="creators2" href="https://github.com/Twmwayne"> Travis Mendenhall </a> | 
+           <a className="creators3" href="https://github.com/Dirk44"> Derrick DeVilliers </a>
+           
+          </div>
         </div>
-      </div>
+      </Container>
     </footer>
   );
 }
--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React from "react"
 import '../../App.css';
 import { Button } from 'reactstrap'
+import Footer from '../../components/Footer/index';
 
 
 
@@ -8,5 +9,6 @@ import { Button } from 'reactstrap'
 export default function Dashboard() {
   return <div className='dashboard'> 
     <Button href="/game-play" variant="primary">Play Game</Button>
+    <Footer />
   </div>;
 }
