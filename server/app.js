b2cb447ca22ffc9d13a3d21bb09c852e8d4d3b4b
John Robinson <johnarobinson133@gmail.com>
2021-02-27 12:45:18 -0700

    added ../ to client in app.js in server

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use('/posts', postRoutes);
 if (process.env.NODE_ENV === "production") {
   // app.use(express.static(path.join(__dirname, "../client/build")))
   app.use(express.static("client/build"))
-  app.get("*",(req,res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")))
+  app.get("*",(req,res) => res.sendFile(path.resolve(__dirname, "../client", "build", "index.html")))
 }
 
 const CONNECTION_URL = 'mongodb+srv://jrobi133:Hunter15@workoutdb.izarp.mongodb.net/WorkoutDB?retryWrites=true&w=majority';
