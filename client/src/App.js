ef94ad63daf40d62052f3caba04d944bb0e5504a
John Robinson <johnarobinson133@gmail.com>
2021-03-02 22:06:21 -0700

    connected data from DB to game play

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,39 @@
 import React, { useState, useEffect } from "react"
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { useSelector, useDispatch } from 'react-redux';
+import { getPosts } from './actions/posts';
+import { useAuth0 } from "@auth0/auth0-react";
+
+// Components 
+import Loading from "./components/loading"
+import Navbar from './components/Nav/Navbar';
+import Footer from './components/Footer/index';
+import ProtectedRoute from "./auth/protected-route"
+
+// Pages 
+import Profile from "./pages/profile/profile"
 import Dashboard from "./pages/Dashboard/Dashboard"
 import HighScores from "./pages/Highscores/Highscores"
 import GameManager from "./pages/GameManager/GameManager"
-import Navbar from './components/Nav/Navbar';
-import Profile from "./pages/profile/profile"
-import ProtectedRoute from "./auth/protected-route"
-import { useAuth0 } from "@auth0/auth0-react";
-import Loading from "./components/loading"
 import GamePlay from "./pages/GamePlay/GamePlay"
-import Footer from './components/Footer/index';
-
 
 const App = () => {
   const { isLoading, isAuthenticated } = useAuth0();
   const [reload, setReload] = useState(false);
   const [loggedIn, setLoggedIn] = useState(false);
+  const allHeroes = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
   
   useEffect(() => {
     setLoggedIn(isAuthenticated); 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [reload, isLoading]);
+  
+  useEffect(() => {
+    dispatch(getPosts());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dispatch]); // allHeroes
 
   if (isLoading) {
     return <Loading />;
@@ -32,7 +44,14 @@ const App = () => {
         <Navbar loggedIn={loggedIn} setReload={setReload} />
         <Switch>
           <ProtectedRoute exact path="/highscores" component={HighScores} />
-          <ProtectedRoute exact path="/game-play" component={GamePlay} />
+          <ProtectedRoute exact path="/game-play" component={(props) => allHeroes.length === 0 ? 
+             <Loading /> : <GamePlay 
+            {...props} 
+            heroes={allHeroes}
+            pageTitle="Game Play"
+            game_message=""
+            // highScore={} // extract users high score here maybe 
+          />} />
           <Route exact path="/" component={Dashboard} />
           <Route path="/game-manager" component={GameManager} />
           <Route path="/profile" component={Profile} />
